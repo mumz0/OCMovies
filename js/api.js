1d719323106a5b9cd154fa3f0d9b62543f2525bb
muumz <parisot.sa@gmail.com>
2023-12-30 03:41:21 +0100

    carousel done

---
+++ js/api.js
@@ -0,0 +1,81 @@
+import * as utils from "./utils.js";
+
+const title_url = "http://localhost:8000/api/v1/titles/";
+const type_url = "http://localhost:8000/api/v1/genres/";
+const sortByImdbScore = "sort_by=-imdb_score";
+const movieType = "genre=";
+
+let allItems = [];
+
+export async function getBestMovie() {
+    try {
+        const response = await fetch(title_url + "?" + sortByImdbScore);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function getMovies(type) {
+    try {
+        const response = await fetch(title_url + "?" + movieType + type + "&" + sortByImdbScore + "&page_size=7");
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function getBestRankedMovies() {
+    try {
+        const response = await fetch(title_url + "?" + sortByImdbScore + "&page_size=7");
+        const data = await response.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
+
+export async function getRandomTypes() {
+    try {
+        const allItems = await fetchPage(type_url);
+
+        if (allItems) {
+            const result = utils.getRandomElements(allItems);
+            return result;
+        }
+    } catch (error) {
+        console.error("Une erreur est survenue lors de la récupération des genres:", error);
+        throw error;
+    }
+}
+
+export async function fetchPage(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        allItems = allItems.concat(data.results);
+
+        if (data.next) {
+            await fetchPage(data.next);
+        }
+
+        return allItems;
+    } catch (error) {
+        console.error("Error fetching page:", error);
+        throw error;
+    }
+}
+
+export async function getMovieDetails(id) {
+    try {
+        const response = await fetch(title_url + id);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        return null;
+    }
+}
---
+++ js/ui/modal.js
@@ -0,0 +1,105 @@
+export function displayModal(movieObj, elemClassName) {
+    console.log(movieObj);
+
+    // Récupérer l'élément modal container
+    const modal = document.getElementById('myModal');
+
+    // Créer le contenu du modal
+    const modalContainer = document.createElement('div');
+    modalContainer.classList.add('modal-container');
+
+    const iconElement = document.createElement('i')
+    iconElement.classList.add('fas', 'fa-sharp', 'fa-regular', 'fa-xmark');
+    modalContainer.appendChild(iconElement);
+
+    // Ajouter le titre du film au contenu du modal
+    const titleElement = document.createElement('h1');
+    titleElement.textContent = movieObj.title;
+    modalContainer.appendChild(titleElement);
+
+    const modalContent = document.createElement('div');
+    modalContent.classList.add('modal-content');
+    const infosElement = document.createElement('div');
+    infosElement.classList.add('movie-infos');
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('img-container');
+
+    // Ajouter l'image du film au contenu du modal
+    const imageElement = document.createElement('img');
+    imageElement.src = movieObj.image_url;
+    imageElement.alt = movieObj.title;
+    imgContainer.appendChild(imageElement);
+
+    // Ajouter le contenu du modal à la modal container
+    modal.appendChild(modalContainer);
+
+    const genreElement = document.createElement('h3');
+    genreElement.textContent = `Genre: ${movieObj.genres.join(', ')}`;
+    infosElement.appendChild(genreElement);
+
+    const dateElement = document.createElement('h3');
+    dateElement.textContent = `Date de parution: ${movieObj.date_published}`;
+    infosElement.appendChild(dateElement);
+
+    const ratedElement = document.createElement('h3');
+    ratedElement.textContent = `Rated: ${movieObj.rated}`;
+    infosElement.appendChild(ratedElement);
+    
+    const imdbScoreElement = document.createElement('h3');
+    imdbScoreElement.textContent = `imdb score: ${movieObj.imdb_score}`;
+    infosElement.appendChild(imdbScoreElement);
+    
+    const directorsElement = document.createElement('h3');
+    directorsElement.textContent = `Réalisateur: ${movieObj.directors}`;
+    infosElement.appendChild(directorsElement);
+
+    const actorsElement = document.createElement('h3');
+    actorsElement.textContent = `Acteurs: ${movieObj.actors.join(', ')}`;
+    infosElement.appendChild(actorsElement);
+
+    const durationElement = document.createElement('h3');
+    durationElement.textContent = `Durée: ${movieObj.duration} min`;
+    infosElement.appendChild(durationElement);
+
+    const countriesElement = document.createElement('h3');
+    countriesElement.textContent = `Pays: ${movieObj.countries}`;
+    infosElement.appendChild(countriesElement);
+
+    const boxOfficeElement = document.createElement('h3');
+    if(movieObj.worldwide_gross_income == null){
+        boxOfficeElement.textContent = `Resultat du box office : inconnu`;
+    }
+    else{
+        boxOfficeElement.textContent = `Resultat du box office : ${movieObj.worldwide_gross_income}`;
+    }
+    infosElement.appendChild(boxOfficeElement);
+
+    const descriptionElement = document.createElement('h3');
+    descriptionElement.textContent = `Description: ${movieObj.description}`;
+    infosElement.appendChild(descriptionElement);
+    
+
+    modalContent.appendChild(imgContainer);
+    modalContent.appendChild(infosElement);
+    modalContainer.appendChild(modalContent);
+    
+    // Afficher la modal
+    modal.style.display = 'flex';
+
+    // Ajouter un événement de clic sur la modal container pour la fermer
+    iconElement.addEventListener('click', function() {
+        // Vérifier si modalContent existe avant de le supprimer
+        if (modalContainer && modalContainer.parentNode === modal) {
+            modal.removeChild(modalContainer);
+        }
+
+        // Cacher la modal
+        modal.style.display = 'none';
+
+        // Supprimer la classe "active" de l'élément avec l'ID "movieImg"
+        var elemToRemove = document.getElementById(elemClassName);
+        if (elemToRemove) {
+            elemToRemove.classList.remove("active");
+        }
+    });
+}
